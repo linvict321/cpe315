50e8a6671f3262e142fb1feba1e94f2567729ca0
linvict321 <86497178+linvict321@users.noreply.github.com>
2026-04-21 23:40:29 -0700

    Add files via upload

--- lab2/Assembler.java
+++ lab2/Assembler.java
@@ -22,9 +22,24 @@ public class Assembler{
             //parse through lines, get rid of white space, comments,
             }
             //opcode table: opcode, machine code, type of opcode, length of opcode
+            //and, or, add, addi, sll, sub, slt, beq, bne, lw, sw, j, jr, and jal
             HashMap<String, Integer> opcode = new HashMap<>();
-            opcode.put("ADD", 00110011);
-            opcode.put("SUB", 00110011);
+            opcode.put("and", 0);
+            opcode.put("or", 0);
+            opcode.put("add", 0);
+            opcode.put("addi", 8);
+            opcode.put("sll", 0);
+            opcode.put("sub", 0);
+            opcode.put("slt", 0);
+            opcode.put("beq", 4);
+            opcode.put("bne", 5);
+            opcode.put("lw", 35);
+            opcode.put("sw", 43);
+            opcode.put("j", 2);
+            opcode.put("jr", 0);
+            opcode.put("jal", 3);
+
+
 
             //register table: register name, machine constant
             //no need for $at, $k0, $k1, $gp, $fp.
@@ -45,15 +60,21 @@ public class Assembler{
             registerTable.put("$t6", 14);
             registerTable.put("$t7", 15);
             registerTable.put("$s0", 16);
-            registerTable.put("$s0", 16);
-            registerTable.put("$s0", 16);
-            registerTable.put("$s0", 16);
-            registerTable.put("$s0", 16);
+            registerTable.put("$s1", 17);
+            registerTable.put("$s2", 18);
+            registerTable.put("$s3", 19);
+            registerTable.put("$s4", 20);
+            registerTable.put("$s5", 21);
+            registerTable.put("$s6", 22);
+            registerTable.put("$s7", 23);
+            registerTable.put("$t8", 24);
+            registerTable.put("$t9", 25);
+            registerTable.put("$sp", 29);
+            registerTable.put("$ra", 31);
 
             //symbol table: symbol num, name, addr, length
             HashMap<String, Integer> symbolTable = new HashMap<>();
 
-            //literal table: literal no, literal, literal address
 
 
         }
