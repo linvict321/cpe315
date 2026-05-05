66d1ca7facd637bc20460f84db9dce7dbbcf3678
Ryo Nakahodo Sannomiya <ryosannomiya1@gmail.com>
2026-05-04 22:11:53 -0700

    more formatting 1 updates

--- lab3/lab3.java
+++ lab3/lab3.java
@@ -411,13 +411,12 @@ public class lab3{
                 System.out.println("pc = " + pc);
 
                 // also find format (imma just manually do it)
-                System.out.println("$0 = " + registers[0] + "\t\t$v0 = " + registers[2] + "\t\t$v1 = " + registers[3] + "\t\t$a0 = " + registers[4]);
-                System.out.println("$a1 = " + registers[5] + "\t\t$a2 = " + registers[6] + "\t\t$a3 = " + registers[7] + "\t\t$t0 = " + registers[8]);
-                System.out.println("$t1 = " + registers[9] + "\t\t$t2 = " + registers[10] + "\t\t$t3 = " + registers[11] + "\t\t$t4 = " + registers[12]);
-                System.out.println("$t5 = " + registers[13] + "\t\t$t6 = " + registers[14] + "\t\t$t7 = " + registers[15] + "\t\t$s0 = " + registers[16]);
-                System.out.println("$s1 = " + registers[17] + "\t\t$s2 = " + registers[18] + "\t\t$s3 = " + registers[19] + "\t\t$s4 = " + registers[20]);
-                System.out.println("$s5 = " + registers[21] + "\t\t$s6 = " + registers[22] + "\t\t$s7 = " + registers[23] + "\t\t$t8 = " + registers[24]);
-                System.out.println("$t9 = " + registers[25] + "\t\t$sp = " + registers[29] + "\t\t$ra = " + registers[31]);
+                System.out.println("$0 = " + registers[0] + "          $v0 = " + registers[2] + "         $v1 = " + registers[3] + "         $a0 = " + registers[4]);                System.out.println("$a1 = " + registers[5] + "\t\t$a2 = " + registers[6] + "\t\t$a3 = " + registers[7] + "\t\t$t0 = " + registers[8]);
+                System.out.println("$t1 = " + registers[9] + "          $t2 = " + registers[10] + "          $t3 = " + registers[11] + "          $t4 = " + registers[12]);
+                System.out.println("$t5 = " + registers[13] + "          $t6 = " + registers[14] + "          $t7 = " + registers[15] + "          $s0 = " + registers[16]);
+                System.out.println("$s1 = " + registers[17] + "          $s2 = " + registers[18] + "          $s3 = " + registers[19] + "          $s4 = " + registers[20]);
+                System.out.println("$s5 = " + registers[21] + "          $s6 = " + registers[22] + "          $s7 = " + registers[23] + "          $t8 = " + registers[24]);
+                System.out.println("$t9 = " + registers[25] + "          $sp = " + registers[29] + "          $ra = " + registers[31]);
                 System.out.println();
             }
             else if(cmd.equals("s")){
