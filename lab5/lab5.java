17075c965702d51c833a3d733958071588307a60
Ryo Nakahodo Sannomiya <ryosannomiya1@gmail.com>
2026-05-28 10:39:28 -0700

    new updates 2

--- lab5/lab5.java
+++ lab5/lab5.java
@@ -511,7 +511,7 @@ public class lab5{
                 }
                 //display prediction stats
                 System.out.println();
-                System.out.printf("accuracy %.2f%% (%d correct predictions, %d predictions) %n", accuracy, correctPredict, totalPredict);
+                System.out.printf("accuracy %.2f%% (%d correct predictions, %d predictions)%n", accuracy, correctPredict, totalPredict);
                 System.out.println();
             }
 
